c8cb4fc2840c8e3d9a2917ea9a05ad48e6bffc0d
veterinarianynvythen <veterinarianynvythen@protonmail.com>
2025-12-16 12:03:11 +0100

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
+
+echo "========================================="
+echo "===            SYSTEM INFO            ==="
+echo "========================================="
+echo "[*] whoami & id:"
+whoami
 id
+echo "\n[*] Hostname:"
+hostname
+echo "\n[*] Linux Version:"
+uname -a
+echo "\n[*] Filesystem Root:"
+ls -la /
+echo ""
